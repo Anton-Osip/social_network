d75b1c968f770fdcef49231a0bb9ee286034dcb6
Anton-Osip <osipchika@gmail.com>
2024-06-29 20:39:12 +0300

    20. Уроки React JS (NavLink) - react курсы бесплатно

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Navbar: React.FC = () => {
     return (
-        <nav className={styles.nav}>
+        <nav className = {styles.nav}>
             <ul className = {styles.list}>
-                <li><a href = "/profile" className = {styles.item}>Profile</a></li>
-                <li><a href = "/dialogs" className = {styles.item}>Messages</a></li>
-                <li><a href = "/news" className = {styles.item}>News</a></li>
-                <li><a href = "/music" className = {styles.item}>Music</a></li>
+                <li>
+                    <NavLink
+                        to = "/profile"
+                        className = {styles.item}
+                        activeClassName = {styles.active}>
+                        Profile
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to = "/dialogs"
+                        className = {styles.item}
+                        activeClassName = {styles.active}>
+                        Messages
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to = "/news"
+                        className = {styles.item}
+                        activeClassName = {styles.active}>
+                        News
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to = "/music"
+                        className = {styles.item}
+                        activeClassName = {styles.active}>
+                        Music
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     )
